9444ca52b58500028580849e95bf7f256073df9d
dcqljw <1711708217@qq.com>
2023-03-29 17:17:33 +0800

    Update config.py

--- dcqchat_server/config/config.py
+++ dcqchat_server/config/config.py
@@ -1,8 +1,8 @@
-HOST = '8.210.229.44'
+HOST = ''
 PORT = '3306'
-DATABASE = 'dcqchat'
-USERNAME = 'root'
-PASSWORD = 'dcq.159357'
+DATABASE = ''
+USERNAME = ''
+PASSWORD = ''
 
 DB_URI = "mysql+pymysql://{username}:{password}@{host}:{port}/{db}?charset=utf8".format(username=USERNAME, password=PASSWORD, host=HOST, port=PORT, db=DATABASE)
 
