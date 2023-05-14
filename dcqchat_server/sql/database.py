061edaa4bbf2dfe94386cd704bb1a88cddb8fe84
dcqljw <1711708217@qq.com>
2023-05-14 20:36:23 +0800

    Update database.py

--- dcqchat_server/sql/database.py
+++ dcqchat_server/sql/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:mysql.159357@8.210.229.44:3306/dcqchat?charset=utf8"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:password@ip:3306/dcqchat?charset=utf8"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
